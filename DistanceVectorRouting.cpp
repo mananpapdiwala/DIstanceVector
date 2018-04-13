cf5e0fdf8e3650c26d230db47a2c0b9664b10c50
Manan Vinod Papdiwala <manpapdi@lab009h08.soic.indiana.edu>
2018-04-13 18:04:57 -0400

    Added error handler.

--- DistanceVectorRouting.cpp
+++ DistanceVectorRouting.cpp
@@ -1,14 +1,22 @@
 #include <iostream>
 #include <cstdlib>
 
+#define DEFAULT_PARAMS 7
+
 using namespace std;
 
+void error_handler(string message){
+	cerr<<message<<endl;
+	cerr<<"Shutting down"<<endl;
+	exit(EXIT_FAILURE);
+};
+
 int main(int argc, char const* argv[]){
-	if(argc != 7){
+	if(argc != DEFAULT_PARAMS){
 		cout<<"The program takes 7 arguments."<<endl;
 		cout<<"Please run the program using format:"<<endl;
 		cout<<"DistanceVectorRouting <config> <portnumber> <TTL> <infinity> <Period> <Poison Reverse>"<<endl;
-		exit(EXIT_FAILURE);
+		error_handler("Invalid number of arguments");
 	}
 
 	string configFile = string(argv[1]);
@@ -17,6 +25,7 @@ int main(int argc, char const* argv[]){
 	int infinity = atoi(argv[4]);
 	int period = atoi(argv[5]);
 	int poisonReverse = atoi(argv[6]);
+
 
 	cout<<configFile<<endl;
 	cout<<portNumber<<endl;
